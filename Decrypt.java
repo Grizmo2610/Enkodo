17fc0dd6272e2f1736ea2cd5850222974bf09f46
Grizmo2610 <90567545+Grizmo2610@users.noreply.github.com>
2023-08-02 16:39:57 +0700

    Add files via upload

---
+++ Decrypt.java
@@ -0,0 +1,13 @@
+public class Decrypt {
+    /***
+     * Giải mã đoạn text
+     *
+     *
+     * @param cipherText Đoạn văn bản cần giải mã
+     * @param privateKey Khóa riêng tư để giải mã
+     * @return Đoạn văn bản được giải
+     */
+    public static String decrypt(int[] cipherText, int[] privateKey){
+        return "Result";
+    }
+}
