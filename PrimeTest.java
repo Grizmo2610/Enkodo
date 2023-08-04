d1beeb6b2512edf22914e46698e71ca128c3bbbb
Grizmo2610 <90567545+Grizmo2610@users.noreply.github.com>
2023-08-04 23:22:47 +0700

    Delete PrimeTest.java

--- PrimeTest.java
+++
@@ -1,8 +0,0 @@
-public class PrimeTest {
-    /**
-     * Kiểm tra xem số được nhập có phải số nguyên tố không?
-     * */
-    public static boolean checkPrime(int number){
-        return true;
-    }
-}
